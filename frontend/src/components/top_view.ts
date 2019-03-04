3a1bcae29dc398e4c13eb7d75039fc04d7dbfb66
Constance Ye <constanceye1316@gmail.com>
2019-03-04 00:55:05 -0500

    fixes #4

--- frontend/src/components/top_view.ts
+++ frontend/src/components/top_view.ts
@@ -9,8 +9,8 @@ export function topView(state: choo.IState, emit: Emit) {
     return html`
     <div id="top-container">
     <div id="top">
-
         <div id="cat">Mldraw!</div>
+        <div id="made-by">Made by <a href="http://www.aman.work/">Aman</a> and <a href="https://connieye.com">Connie</a> 	(„• ֊ •„)</div>
     </div></div>`
 }
 
